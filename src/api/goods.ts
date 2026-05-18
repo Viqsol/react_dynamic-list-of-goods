0f4da913c3145e8942e0a0bfe5aabb147f94b349
Viktor Gulko <viq@ukr.net>
2026-05-18 22:03:18 +0300

    fixed error bug requirement from Luke

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,13 +4,17 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to fetch goods');
-    }
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Server responded with an error');
+      }
 
-    return response.json();
-  });
+      return response.json();
+    })
+    .catch(() => {
+      throw new Error('Failed to fetch goods');
+    });
 }
 
 export const get5First = () => {
